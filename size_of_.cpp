1c5306510be31dfd205e89c848d7a3d0e506d5f5
laeeqentc <laeeqentc@gmail.com>
2025-01-14 19:37:00 +0530

    size of operator cpp more added

--- size_of_.cpp
+++ size_of_.cpp
@@ -4,10 +4,18 @@ int main()
 {
 cout << "Size of char : " << sizeof(char) << endl;
 cout << "Size of unsigned char : " << sizeof(unsigned char) << endl;
+cout << "Size of signed char : " << sizeof(signed char) << endl;
 cout << "Size of int : " << sizeof(int) << endl;
+cout << "Size of unsigned int : " << sizeof(unsigned int) << endl;
+cout << "Size of signed int : " << sizeof(signed int) << endl;
 cout << "Size of short int : " << sizeof(short int) << endl;
 cout << "Size of long int : " << sizeof(long int) << endl;
+//double long int i;
+//cout << "Size of double int : " << sizeof(double long int) << endl;
 cout << "Size of float : " << sizeof(float) << endl;
+//cout << "Size of signed float : " << sizeof(unsigned float) << endl;
+//cout << "Size of unsigned float : " << sizeof(unsigned float) << endl;
+
 cout << "Size of double : " << sizeof(double) << endl;
 cout << "Size of wchar_t : " << sizeof(wchar_t) << endl;
 return 0;
